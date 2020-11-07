41366bc3e2b3d1a3c946bc2689c3b3bc08d7e991
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-07 15:08:07 -0800

    added some features to the navbar

--- classify/src/App.js
+++ classify/src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Navbar></Navbar>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ classify/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+//Login, account, playlist, playlists, search,
+function Navbar() {
+	return (
+		<>
+			<nav className="navbar">
+				<div className="navbar-container">
+					<Link to="/" className="navbar-logo">
+						Home
+					</Link>
+					<ul className="nav-menu">
+						<li>
+							<Link to="/login" className="nav-links">
+								Login
+							</Link>
+						</li>
+						<li>
+							<Link to="/account" className="nav-links">
+								Account
+							</Link>
+						</li>
+						<li>
+							<Link to="/playlist" className="nav-links">
+								Playlist
+							</Link>
+						</li>
+						<li>
+							<Link to="/search" className="nav-links">
+								Search
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</>
+	);
+}
+export default Navbar;
