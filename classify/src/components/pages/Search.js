07425fa36edb7468cd650ebdd349976d6d742e8e
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-07 17:32:07 -0800

    implemented login and search

--- classify/src/components/pages/Search.js
+++ classify/src/components/pages/Search.js
@@ -1,6 +1,38 @@
 import React from "react";
-import "../../App.css";
 
 export default function Search() {
-	return <h1 className="search">Search</h1>;
+	return (
+		<>
+			<h1>Search</h1>
+			<div>
+				<span>Title</span>
+				<span>Artist</span>
+				<span>Actions</span>
+			</div>
+
+			<div>
+				<span>Foo</span>
+				<span>Bar</span>
+				<span>
+					<button>Add</button>
+				</span>
+			</div>
+
+			<div>
+				<span>Bar</span>
+				<span>Baz</span>
+				<span>
+					<button>Add</button>
+				</span>
+			</div>
+
+			<div>
+				<span>Foobar</span>
+				<span>Baz</span>
+				<span>
+					<button>Add</button>
+				</span>
+			</div>
+		</>
+	);
 }
