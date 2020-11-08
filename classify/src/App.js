c5f094ce6be65f61e80834594f2ad66567fa2695
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-07 17:55:20 -0800

    fix search

--- classify/src/App.js
+++ classify/src/App.js
@@ -7,9 +7,8 @@ import Login from "./components/pages/Login";
 import Account from "./components/pages/Account";
 import Playlists from "./components/pages/Playlists";
 import Playlist from "./components/pages/Playlist";
-import Search from "./components/pages/Search";
 import Admin from "./components/pages/Admin";
-
+import Search from "./components/pages/Search";
 
 function App() {
 	return (
@@ -22,8 +21,8 @@ function App() {
 					<Route path="/account" component={Account} />
 					<Route path="/playlists" component={Playlists} />
 					<Route path="/playlist" component={Playlist} />
-					<Route path="/search" componenet={Search} />
 					<Route path="/admin" component={Admin} />
+					<Route path="/search" component={Search} />
 				</Switch>
 			</Router>
 		</div>
