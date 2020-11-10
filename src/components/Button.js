dd24aa1a34c5b81b7aade9e74a665813d2b81d24
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-09 16:36:58 -0800

    added signup

--- src/components/Button.js
+++ src/components/Button.js
@@ -43,3 +43,29 @@ export const Button = ({
 		</Link>
 	);
 };
+
+export const SignUpButton = ({
+	children,
+	type,
+	onClick,
+	buttonStyle,
+	buttonSize,
+}) => {
+	const checkButtonStyle = STYLES.includes(buttonStyle)
+		? buttonStyle
+		: STYLES[0];
+
+	const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+	return (
+		<Link to="/sign-up" className="btn-mobile">
+			<button
+				className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+				onClick={onClick}
+				type={type}
+			>
+				{children}
+			</button>
+		</Link>
+	);
+};
