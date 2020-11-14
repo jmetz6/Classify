5379f3865636f8dfb05491417d783d2466053ace
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-13 22:00:13 -0800

    heroku test #6

---
+++ client/components/pages/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../../App.css";
+
+export default function Login() {
+	return (
+		<div className="login">
+			<h1>Login</h1>
+			<div className="login-text">
+				<div className="username">
+					<label for="username">Username</label>
+					<input type="text" />
+				</div>
+				<div>
+					<label for="password">Password</label>
+					<input type="text" />
+					<span>{<button>Login</button>}</span>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ client/components/pages/Songs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../../App.css";
+import Table from "../Table";
+
+export default function Songs() {
+
+	let cols = ["Name", "Actions"];
+    let data = [
+        { id: 1, name: "Symphony No. 5 in C Minor, Op. 67: 1. Allegro con brio", actions: ["add"] },
+		{ id: 2, name: "Symphony No. 5 in C Minor, Op. 67: 2. Andante con moto", actions: ["add"] },
+		{ id: 3, name: "Symphony No. 5 in C Minor, Op. 67: 3. Allegro", actions: ["add"] },
+		{ id: 4, name: "Symphony No. 5 in C Minor, Op. 67: 4. Allegro", actions: ["add"] },
+    ]
+
+	return (
+	
+		<div className="songs flex-page">
+			<Table
+				title="Songs"
+				cols={cols}
+				data={data}
+				property="song"
+			>
+			</Table>
+
+		</div>
+	
+	)
+}
