f569b36b424d0b48a77a07d96b8081f11fb19b03
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-15 12:42:58 -0800

    add sql

---
+++ dataManipulationQueries.sql
@@ -0,0 +1,55 @@
+--users
+--add
+INSERT INTO `users` (`username`, `password`) VALUES 
+(:usernameInput, :passwordInput);
+
+--remove
+DELETE FROM `users` WHERE `username`=:usernameInput;
+
+--playlists
+--add
+INSERT INTO `playlists` (`name`, `user`) VALUES 
+(:playlistNameInput, (SELECT `id` FROM `users` WHERE `name`=:usernameInput));
+
+--remove
+DELETE FROM `playlists` WHERE `name`=:playlistNameInput;
+
+--songs
+--add
+INSERT INTO `songs` (`name`) VALUES 
+(:songNameInput);
+
+INSERT INTO `artists` (`name`) VALUES 
+(:songArtistName);
+
+INSERT INTO `song_artist_association` (`songID`, `artistID`) VALUES 
+((SELECT `id` FROM `songs` WHERE `name`=:songNameInput), (SELECT `id` FROM `artists` WHERE `name`=:songArtistName));
+
+--remove
+DELETE FROM `songs` WHERE `name`=:songNameInput;
+
+--artists
+--add
+INSERT INTO `artists` (`name`) VALUES 
+(:artistNameInput);
+
+--remove
+DELETE FROM `artists` WHERE `name`=:artistNameInput;
+
+--add song to playlist
+INSERT INTO `playlist_song_association` (`playlistID`, `songID`) VALUES 
+((SELECT `id` FROM `playlists` WHERE `name`=:playlistNameInput), (SELECT `id` FROM `songs` WHERE `name`=:songNameInput));
+
+--delete from from playlist
+DELETE FROM `playlist_song_association` WHERE 
+	`playlistID`=(SELECT `id` FROM `playlists` WHERE `name`=:playlistNameInput) AND 
+	`songID`=(SELECT `id` FROM `songs` WHERE `name`=:songNameInput);
+	
+--select playlist
+SELECT * FROM `playlist_song_association` WHERE `playlistID`=(SELECT `id` FROM `playlists` WHERE `name`=:playlistNameInput);
+	
+--song search
+SELECT * FROM `songs` WHERE `name`=:searchText;
+
+--artist search
+SELECT * FROM `artists` WHERE `name`=:searchText;
