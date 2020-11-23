132c570131bc9148b77bd602119bca750a8e52b9
Arthur Hiew <ahfx8518@hotmail.com>
2020-11-22 23:28:35 -0800

    admin auto populate

--- react-ui/src/components/pages/Admin.js
+++ react-ui/src/components/pages/Admin.js
@@ -1,30 +1,55 @@
-import React from "react";
+import Axios from "axios";
+import React, { Component } from "react";
 import "../../App.css";
 import Table from "../Table";
 
+export default class Admin extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			cols: ["Username", "Playlists", "Actions"],
+			data: ["edit", "remove"],
+		};
+	}
 
-export default function Admin() {
-    
-    let cols = ["Username", "Playlists", "Actions"];
-    let data = [
-        { id: 1, username: "user123", playlists: "2", actions: ["edit", "remove"] },
-        { id: 2, username: "user456", playlists: "1", actions: ["edit", "remove"] }
-    ]
+	// let cols = ["Username", "Playlists", "Actions"];
+	// let data = [
+	//     { id: 1, username: "user123", playlists: "2", actions: ["edit", "remove"] },
+	//     { id: 2, username: "user456", playlists: "1", actions: ["edit", "remove"] }
+	// ]
+	componentDidMount() {
+		let data = [];
+		const apiUrl = "http://localhost:5000/api/admin";
+		Axios.post(apiUrl).then((result) => {
+			// debugger;
+			console.log(result);
+			if (!result.data.length) {
+				alert("Error retrieving users");
+			} else {
+				data = result.data;
+				data.forEach((s) => {
+					s.actions = ["Username", "Playlists", "Actions"];
+				});
+				this.setState({ data });
+			}
+		});
+	}
+	render() {
+		return (
+			<div>
+				<div className="admin flex-page">
+					<div>
+						<button className="btn btn-primary">Add new user</button>
+					</div>
 
-	return (
-		<div>
-			<div className="admin flex-page">
-                <div>
-                    <button className="btn btn-primary">Add new user</button>
-                </div>
-                
-                <Table
-                    title="Users"
-                    cols={cols}
-                    data={data}
-                    property="user"
-                ></Table>
-            </div>
-		</div>
-	);
+					<Table
+						title="Users"
+						cols={this.state.cols}
+						data={this.state.data}
+						property="user"
+					></Table>
+				</div>
+			</div>
+		);
+	}
 }
