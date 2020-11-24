f8c6ffd44ebbf70ffd92e6c241faae58a47eebc8
jmetz6 <metzjo@oregonstate.edu>
2020-11-23 22:26:31 -0800

    Form work :(

--- react-ui/src/components/pages/Admin.js
+++ react-ui/src/components/pages/Admin.js
@@ -1,7 +1,34 @@
 import Axios from "axios";
 import React, { Component } from "react";
-import "../../App.css";
+
 import Table from "../Table";
+import { InputForm } from "../Form";
+
+import "../../App.css";
+
+const SendForm = (form) => {
+	let tform = document.getElementById(form);
+	tform.submit();
+	let apiUrlForm = "/api/signup";
+	let url = window.location.href.split("?")[1];
+	let data = url.split("&");
+	let sendData = {
+		name: data[0].split("=")[1],
+		word: data[1].split("=")[1]
+	}
+
+	Axios.post(apiUrlForm, sendData).then((result) => {
+		// debugger;
+		console.log(result);
+		if (result.data.errno) { 
+			alert("Failure: Invalid User");
+		}
+		else {
+			alert("Add user successful");
+		}
+
+	});
+};
 
 export default class Admin extends Component {
 	constructor(props) {
@@ -9,8 +36,24 @@ export default class Admin extends Component {
 		this.state = {
 			cols: ["Username", "Password", "Actions"],
 			data: [],
+			render: ''
 		};
 	}
+
+	handleClick(compName, e){
+		if(this.state.render === compName) {
+			this.setState({render: ''});
+		}
+		else {
+        	this.setState({render: compName});
+		}
+	}
+	_renderSubComp(){
+        switch(this.state.render){
+			case 'AdminForm': return <AdminForm />
+			default: return;
+		}
+    }
 
 	componentDidMount() {
 		let data = [];
@@ -29,12 +72,14 @@ export default class Admin extends Component {
 			}
 		});
 	}
+
 	render() {
 		return (
 			<div>
-				<div className="admin flex-page">
+				<div className="admin flex-page flex-page-column">
 					<div>
-						<button className="btn btn-primary">Add new user</button>
+						<button onClick={this.handleClick.bind(this, 'AdminForm')} className="btn btn-primary">Add new user</button>
+						{this._renderSubComp()}		
 					</div>
 
 					<Table
@@ -47,4 +92,16 @@ export default class Admin extends Component {
 			</div>
 		);
 	}
+}
+
+class AdminForm extends React.Component {
+    render() {
+        return (
+			<div> 
+				<InputForm onClick={SendForm} title="Add User" inputs={ [ 'username', 'password' ] } selects={ [] }>
+				
+				</InputForm>
+			</div>	
+		)
+    }
 }
